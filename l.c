c68c2227f1eccb32373562ffd4945512a58771c4
Екатерина Когтева <kkogteva6@gmail.com>
2026-04-01 11:59:24 +0300

    req

--- l.c
+++ l.c
@@ -1,74 +1,44 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/shm.h>
+#include <sys/sem.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-#define ITERATIONS 20
-#define DELAY_COUNT 100000000
-
-// Функция искусственной задержки
-void delay() {
-    volatile long i;
-    for (i = 0; i < DELAY_COUNT; i++);
-}
+// структура для semctl
+union semun {
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+};
 
 int main() {
-    key_t key;        
-    int shmid;        
-    int *counter;     // указатель на общий счетчик в разделяемой памяти
+    key_t key;
+    int semid;
+    union semun arg;
 
     // Генерация ключа
-    // Ключ должен быть таким же, как в progA.c
-    // Поэтому используем тот же файл и тот же символ проекта
-    key = ftok("progA.c", 'R');
+    key = ftok("progA.c", 'S');  // отдельный ключ для семафора
     if (key == -1) {
         perror("ftok");
         exit(EXIT_FAILURE);
     }
-    printf("progB [PID=%d]: ключ = 0x%x\n", getpid(), key);
+    printf("Ключ семафора: 0x%x\n", key);
 
-    // Получение доступа к уже существующему сегменту
-    shmid = shmget(key, sizeof(int), 0);
-    if (shmid == -1) {
-        perror("shmget");
-        printf("progB [PID=%d]: сегмент не существует, сначала запустите progA\n", getpid());
+    // Создание массива из 1 семафора
+    semid = semget(key, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    if (semid == -1) {
+        perror("semget");
         exit(EXIT_FAILURE);
     }
-    printf("progB [PID=%d]: подключение к существующему сегменту\n", getpid());
+    printf("Семафор создан, semid = %d\n", semid);
 
-    // Присоединение сегмента к адресному пространству процесса
-    counter = (int *)shmat(shmid, NULL, 0);
-    if (counter == (int *)-1) {
-        perror("shmat");
+    // Инициализация значением 1
+    arg.val = 1;
+    if (semctl(semid, 0, SETVAL, arg) == -1) {
+        perror("semctl");
         exit(EXIT_FAILURE);
     }
-
-    // Цикл работы со счетчиком
-    // - выводим текущее значение
-    // - делаем задержку
-    // - увеличиваем счетчик на 1
-    // - выводим новое значение
-    for (int i = 0; i < ITERATIONS; i++) {
-        printf("progB [PID=%d]: итерация %d, текущее значение = %d\n",
-               getpid(), i + 1, *counter);
-
-        delay();
-
-        (*counter)++;
-
-        printf("progB [PID=%d]: итерация %d, новое значение = %d\n",
-               getpid(), i + 1, *counter);
-    }
-
-    // 5. Отсоединение сегмента
-    if (shmdt(counter) == -1) {
-        perror("shmdt");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("progB [PID=%d]: сегмент отсоединен\n", getpid());
+    printf("Семафор инициализирован значением 1\n");
 
     return 0;
 }
